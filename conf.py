60ac96e575adc30726838ed5922a0fc835bcabd4
xuweio <xuweio@gmail.com>
2020-02-07 22:59:46 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "生活碎碎念"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "250life"
+email = "xuweio@gmail.com"
+author_homepage = "https://www.250life.com"
+description = "Better Tech，Better Life"
+key_words = ['Maverick', '250life', '生活', 'blog']
 language = 'zh-CN'
 external_links = [
     {
@@ -61,17 +61,17 @@ nav = [
 social_links = [
     {
         "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
+        "url": "https://twitter.com/xuweio",
         "icon": "gi gi-twitter"
     },
     {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/xuweio",
         "icon": "gi gi-github"
     },
     {
         "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
+        "url": "https://weibo.com/xuweio/",
         "icon": "gi gi-weibo"
     }
 ]
